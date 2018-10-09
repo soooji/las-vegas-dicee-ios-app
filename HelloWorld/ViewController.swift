64575b0b08f9b02aa2d2bb387ee618a1afc33101
Sajad Beheshti <beheshtisajad@gmail.com>
2018-10-09 09:51:02 +0330

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by NIC on 10/9/18.
+//  Copyright © 2018 Sooji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
